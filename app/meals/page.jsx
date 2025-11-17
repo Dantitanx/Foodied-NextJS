631f340a14cd5d38b8d23d07f3ff234384e98928
Dantitanx <email.daniel.illes@gmail.com>
2025-11-17 18:38:38 +0100

    Handling not found

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -2,10 +2,15 @@ import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css';
 import Link from 'next/link';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +25,9 @@ export default async function MealsPage() {
           <Link href='/meals/share'>Share your favourite recipe</Link>
         </p>
         <main className={classes.main}>
-          <MealsGrid meals={meals} />
+          <Suspense fallback={<p className={classes.loading}>Fetching Meals...</p>}>
+            <Meals />
+          </Suspense>
         </main>
       </header>
     </>
