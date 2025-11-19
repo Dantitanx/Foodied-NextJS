76574f5704e041e54b73b97962b9411d44dc0e69
Dantitanx <email.daniel.illes@gmail.com>
2025-11-19 19:56:00 +0100

    Getting started with share meals

---
+++ app/meals/not-found.jsx
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return <main className="not-found">
+        <h1>Meal not found</h1>
+        <p>Oops, we could not find the meal you are looking for!</p>
+    </main>
+}
